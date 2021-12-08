ff4cddf95a5220be55e84c205008295f1959220f
alex-keigley <30334379+alex-keigley@users.noreply.github.com>
2021-12-08 09:14:36 -0600

    challenge done

--- static/js/app.js
+++ static/js/app.js
@@ -30,17 +30,21 @@ filters = {};
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    element = d3.select(this)
+    let element = d3.select(this)
 
     // 4b. Save the value that was changed as a variable.
-    elementValue = element.property("value")
+    let elementValue = element.property("value")
 
     // 4c. Save the id of the filter that was changed as a variable.
-    elementID = element.property("id")
+    let elementID = element.property("id")
 
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-    filters[elementID] = elementValue
+    if (elementValue === "") {
+      delete filters[elementID]
+    } else {
+        filters[elementID] = elementValue
+    }
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
@@ -51,14 +55,17 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
+    for(const [id, val] of Object.entries(filters)) {
+      filteredData = filteredData.filter(row => row[id] === val)
+    }
     
-  
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData)
+
   }
   
   // 2. Attach an event to listen for changes to each filter
